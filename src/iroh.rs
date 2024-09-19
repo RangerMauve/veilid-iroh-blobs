b2f5310632093a805d6901096a3d450ef2d7f83c
Mauve Signweaver <mauve+git@mauve.moe>
2024-09-18 23:07:25 -0400

    Progress on exporting streams from iroh

--- src/iroh.rs
+++ src/iroh.rs
@@ -3,17 +3,35 @@ use crate::make_route;
 use crate::tunnels::TunnelManager;
 
 use anyhow::anyhow;
+use anyhow::Ok;
 use anyhow::Result;
+use async_stream::stream;
+use bytes::Bytes;
+use futures_lite::stream;
+use futures_lite::stream::once;
+use futures_lite::stream::unfold;
+use futures_lite::{Stream, StreamExt};
+use futures_util::lock::Mutex;
+use futures_util::pin_mut;
 use iroh_blobs::store::ImportMode;
 use iroh_blobs::store::ImportProgress;
+use iroh_blobs::store::Map;
+use iroh_blobs::store::ReadableStore;
 use iroh_blobs::store::Store;
 use iroh_blobs::util::progress::IgnoreProgressSender;
 use iroh_blobs::BlobFormat;
 use iroh_blobs::Hash;
+use iroh_io::AsyncSliceReader;
+use iroh_io::AsyncSliceReaderExt;
+use std::cmp::min;
+use std::io::Error;
+use std::io::ErrorKind;
 use std::path::Path;
+use std::result;
 use std::str::FromStr;
 use std::{path::PathBuf, sync::Arc};
 use tokio::sync::broadcast::Receiver;
+use tokio::sync::mpsc;
 use tracing::info;
 use veilid_core::{RouteId, RoutingContext, VeilidAPI, VeilidUpdate};
 
@@ -81,6 +99,53 @@ impl VeilidIrohBlobs {
         let hash = tag.hash().clone();
         return Ok(hash);
     }
+
+    pub async fn read_file(&self, hash: Hash) -> Result<mpsc::Receiver<std::io::Result<Bytes>>> {
+        let chunk_size = 1024usize; // TODO: what's a good chunk size for veilid messages?
+
+        let (send, read) = mpsc::channel::<std::io::Result<Bytes>>(2);
+
+        let (reader, size) = self.open_file_reader(hash).await?;
+
+        let reader = Arc::new(Mutex::new(reader));
+
+        tokio::spawn(async move {
+            let index = 0usize;
+            loop {
+                if index >= size {
+                    return;
+                }
+
+                let remaining = size - (index as usize) - chunk_size;
+                let chunk_size = min(chunk_size, remaining);
+                {
+                    let reader = reader.lock().await;
+                    let chunk = reader.read_at(index as u64, chunk_size).await;
+
+                    if send.send(chunk).await.is_err() {
+                        return;
+                    }
+                }
+            }
+        });
+
+        return Ok(read);
+    }
+
+    async fn open_file_reader(
+        &self,
+        hash: Hash,
+    ) -> std::io::Result<(impl AsyncSliceReader, usize)> {
+        let handle = self.store.get(&hash).await?;
+
+        if !handle.is_some() {
+            return std::io::Result::Err(Error::new(ErrorKind::Other, "Unable to get file"));
+        }
+
+        let mut reader = handle.unwrap().data_reader();
+        let size = reader.size().await? as usize;
+        return std::io::Result::Ok((reader, size));
+    }
 }
 
 async fn init_deps(
@@ -115,6 +180,8 @@ async fn test_blobs() {
         .unwrap();
 
     println!("Hash of README: {0}", hash);
+
+    let stream = blobs.read_file(hash).await;
 
     blobs.veilid.shutdown().await;
 }
